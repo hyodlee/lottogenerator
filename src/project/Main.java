4afd3f7c3aadf42ccbdfffbdf279834f52b4441e
Lee hyo duck <darshu@korea.com>
2018-07-14 05:02:32 +0000

    working

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,21 @@
 package project;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
 
     /**
      * @param args
      */
+    // public static void main(String[] args) {
+    //     // TODO Auto-generated method stub
+    //     System.out.println("Hello goorm!");
+    // }
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+        int[] result = new LottoGenerator().generate();
+        System.out.println(Arrays.toString(result));
     }
 
 }
